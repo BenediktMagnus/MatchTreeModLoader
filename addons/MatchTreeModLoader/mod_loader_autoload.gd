d99aa49c069fa518b72ea55a0dce6a3e6a63f30b
Benedikt Magnus <magnus@magnuscraft.de>
2025-01-18 22:10:00 +0100

    Basic functionality (test)

---
+++ addons/MatchTreeModLoader/mod_loader_autoload.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func _init ():
+	print("Mod Loader: Init")
+
+func _ready ():
+	print("Mod Loader: Ready")
+
+	var success := ProjectSettings.load_resource_pack("res://mod.pck")
+	print(success)
+	
+	if success:
+		var ModScene = load("res://mods/mod/mod.tscn")
+		var mod_scene = ModScene.instantiate()
+		add_child(mod_scene)
+		
