56da5b88afe4bac1860e5e65b041695eb5ff2394
Benedikt Magnus <magnus@magnuscraft.de>
2025-03-23 11:44:47 +0100

    Replaced tabs with spaces.

--- addons/MatchTreeModLoader/mod_loader_autoload.gd
+++ addons/MatchTreeModLoader/mod_loader_autoload.gd
@@ -1,16 +1,15 @@
 extends Node
 
 func _init ():
-	print("Mod Loader: Init")
+    print("Mod Loader: Init")
 
 func _ready ():
-	print("Mod Loader: Ready")
+    print("Mod Loader: Ready")
 
-	var success := ProjectSettings.load_resource_pack("res://mod.pck")
-	print(success)
-	
-	if success:
-		var ModScene = load("res://mods/mod/mod.tscn")
-		var mod_scene = ModScene.instantiate()
-		add_child(mod_scene)
-		
+    var success := ProjectSettings.load_resource_pack("res://mod.pck")
+    print(success)
+
+    if success:
+        var ModScene = load("res://mods/mod/mod.tscn")
+        var mod_scene = ModScene.instantiate()
+        add_child(mod_scene)
--- addons/MatchTreeModLoader/plugin.gd
+++ addons/MatchTreeModLoader/plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 const AUTOLOAD_NAME = "MatchTreeModLoader"
 
 func _enable_plugin ():
-	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/MatchTreeModLoader/mod_loader_autoload.gd")
+    add_autoload_singleton(AUTOLOAD_NAME, "res://addons/MatchTreeModLoader/mod_loader_autoload.gd")
 
 func _disable_plugin():
-	remove_autoload_singleton(AUTOLOAD_NAME)
+    remove_autoload_singleton(AUTOLOAD_NAME)
