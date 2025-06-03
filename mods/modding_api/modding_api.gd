546c8632df8f69589234c3b5162d1c9cfc02e8df
Benedikt Magnus <magnus@magnuscraft.de>
2025-06-03 14:10:35 +0200

    The modding API now provides signals for when common scenes are loaded.

--- mods/modding_api/modding_api.gd
+++ mods/modding_api/modding_api.gd
@@ -1,13 +1,20 @@
 extends Node
 class_name ModdingApi
 
+signal level_loaded (level: Node2D)
+signal options_menu_loaded (options_menu: CanvasLayer)
+signal start_menu_loaded (start_menu: CanvasLayer)
+signal world_map_loaded (world_map: Node2D)
+
 func _init ():
     #print("Base Mod: Init")
     pass
 
 func _enter_tree ():
-    #print("Base Mod: Enter Tree")
-    pass
+    get_tree().node_added.connect(_on_node_added_to_scene_tree)
+
+func _exit_tree ():
+    get_tree().node_added.disconnect(_on_node_added_to_scene_tree)
 
 func _ready ():
     #print("Base Mod: Ready")
@@ -16,3 +23,14 @@ func _ready ():
 func _game_ready ():
     #print("Base Mod: Game Ready")
     pass
+
+func _on_node_added_to_scene_tree (node: Node):
+    match node.get_path():
+        ^"/root/baselevel":
+            level_loaded.emit(node)
+        ^"/root/SceneManager/OptionsMenu":
+            options_menu_loaded.emit(node)
+        ^"/root/StartMenu":
+            start_menu_loaded.emit(node)
+        ^"/root/Worldmap":
+            world_map_loaded.emit(node)
