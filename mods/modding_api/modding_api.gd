17d2d7526527f0af93c4580a33377f8bcbe13d00
Benedikt Magnus <magnus@magnuscraft.de>
2025-06-03 21:30:49 +0200

    Modding API: Added signal "on_scene_loaded".

--- mods/modding_api/modding_api.gd
+++ mods/modding_api/modding_api.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name ModdingApi
 
+## Called after any scene (e.i. a PackedScene) has been instantiated and added to the scene tree.
+signal on_scene_loaded (scene_root: Node)
 signal level_loaded (level: Node2D)
 signal options_menu_loaded (options_menu: CanvasLayer)
 signal start_menu_loaded (start_menu: CanvasLayer)
@@ -25,7 +27,13 @@ func _game_ready ():
     pass
 
 func _on_node_added_to_scene_tree (node: Node):
+    if node.scene_file_path.is_empty():
+        return
+
+    on_scene_loaded.emit(node)
+
     match node.get_path():
+        # TODO: Would it be better to check the scene_file_path here instead of get_path?
         ^"/root/baselevel":
             level_loaded.emit(node)
         ^"/root/SceneManager/OptionsMenu":
